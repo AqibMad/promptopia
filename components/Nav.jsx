89ec5df259f08ef6688d57f20515db18263e4214
Aqib <aqib.javeed@mobiwhiz.com>
2023-05-19 16:10:23 +0500

    1:48:48

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,17 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(()=>{
-    const setProviders = async () => {
+    const setUPProviders = async () => {
       const response = await getProviders();
       setProviders(response);
-    }
-    setProviders();
+    } 
+    setUPProviders();
   },[])
 
 
@@ -33,19 +33,18 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
       <div className="sm:flex hidden">
-        {isUserLoggedIn?(
+        {session?.user ?(
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
-            
             <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
             
             <Link href="/profile">
               <Image 
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 alt="Profile"
                 width={37}
                 height={37}
@@ -72,10 +71,10 @@ const Nav = () => {
       </div>
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image 
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               alt="Promptopia"
               width={37}
               height={37}
